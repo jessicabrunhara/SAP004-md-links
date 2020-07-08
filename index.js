d407cd5fc4e18a215996d39f1d9014bea900f134
Jessica Brunhara <brunharaj@gmail.com>
2020-07-07 21:34:43 -0300

    função linkfinder

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+const linkFinder = path => {
+  // Leia todo o conteúdo do arquivo Markdown solicitado
+  const contents = fs.readFileSync(path, 'utf8');
+
+  const links = [];
+
+  // Use regex para extrair textos e URLs dos links do arquivo Markdown
+  const re = /[^!]\[([^[]*)\]\((http[^)]*)\)/g;
+  const matches = contents.matchAll(re);
+  for (match of matches) {
+    const link = {
+      text: match[1],
+      href: match[2],
+      file: path,
+    };
+    links.push(link);
+  }
+
+  return links;
 };
+
+const foundLinks = linkFinder('README.md');
+console.log(foundLinks);
